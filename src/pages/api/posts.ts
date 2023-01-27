b8c62a0f75e91cc6a4ba032d3c4292eb12cc65c1
Yan <yangabriel2012@gmail.com>
2023-01-26 21:20:19 -0300

    fix: trocando if por switch

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,43 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Posts } from '../../firebase'
-
-interface Data {
-id: string 
-}[]
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Posts } from "../../firebase";
 
 export default async function sendPosts(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const id: string | any  = req.query.id
-console.log(id)
-  const snapshot = await Posts.get()
-  console.log(snapshot.docs)
-  const post = snapshot.docs.map((doc) => ({id: doc.id , ...doc.data()}))
-  if (req.method === 'GET') {
-    
-    res.status(200).send(post)
-    return
-  }
-  if (req.method === 'POST') {
-    const data = req.body
-    await Posts.add(data)
-    res.status(201).send({msg: "Post enviado com sucesso!"})
-    return
-  }
-  if (req.method === 'DELETE') {
-    const id: string = req.body.id
-    await Posts.doc(id).delete()
-    res.status(200).send({msg: "Post deletado com sucesso!"})
-    return
-  }
-  if (req.method === 'PUT'){
-    const id: any = req.query.id
-    const data = req.body
-    await Posts.doc(id).update(data)
-    res.status(200).send({msg: "Post atualizado com sucesso!"})
-    return
+  const snapshot = await Posts.get();
+  const post = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  const { body, method } = req;
+  const id: string = body.id;
+  const data = body;
+
+  switch (method) {
+    case "GET":
+      res.status(200).send(post);
+      break;
+
+    case "POST":
+      await Posts.add(data);
+      res.status(201).send({ msg: "Post enviado com sucesso!" });
+      break;
+
+    case "DELETE":
+      await Posts.doc(id).delete();
+      res.status(200).send({ msg: "Post deletado com sucesso!" });
+      break;
+
+    case "PUT":
+      await Posts.doc(id).update(data);
+      res.status(200).send({ msg: "Post atualizado com sucesso" });
+      break;
+    default:
+      res.status(400).end({ msg: "deu ruim" });
   }
 }
-
